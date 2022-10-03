8188a3f53096b70c1c4a2cfc032edf7dddc2096a
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2022-10-03 11:20:47 +0700

    Unfinished, yet. (1)
    Still on progress to show et_nama value to the SecondActivity.

--- app/src/main/java/com/if3b/pindahlayout/SecondActivity.java
+++ app/src/main/java/com/if3b/pindahlayout/SecondActivity.java
@@ -3,12 +3,21 @@ package com.if3b.pindahlayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
+
+    EditText tvNama;
+    String nama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+
+
+        getSupportActionBar().setTitle("Layout B");
+
     }
 }
