59969b96b496162ac46206f3f430ded63d2fc2e4
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2022-10-10 10:26:01 +0700

    Now SecondActivity displayed value from user inpuutted value to the UI
    Added some change, now SecondActivity UI displayed inputted value from MainActivity.

--- app/src/main/java/com/if3b/pindahlayout/MainActivity.java
+++ app/src/main/java/com/if3b/pindahlayout/MainActivity.java
@@ -3,33 +3,42 @@ package com.if3b.pindahlayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button btnPindah;
+    TextView tvWelcome;
     EditText etNama;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().setTitle("Layout A");
+        getSupportActionBar().setTitle("Home");
 
+        tvWelcome = findViewById(R.id.tv_welcome);
         btnPindah = findViewById(R.id.btn_pindah);
-        etNama = findViewById(R.id.et_nama)
+        etNama = findViewById(R.id.et_nama);
+
         btnPindah.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent pindah = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(pindah);
+                String Nama = etNama.getText().toString();
+
+                if (Nama.trim().equals("")) {
+                    etNama.setError("Nama Tidak Boleh Kosong");
+                }
+                else {
+                    Intent pindah = new Intent(MainActivity.this, SecondActivity.class);
+                    pindah.putExtra("inputNama", Nama);
+                    startActivity(pindah);
+                }
             }
         });
-
     }
 }
--- app/src/main/java/com/if3b/pindahlayout/SecondActivity.java
+++ app/src/main/java/com/if3b/pindahlayout/SecondActivity.java
@@ -2,22 +2,26 @@ package com.if3b.pindahlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
-    EditText tvNama;
-    String nama;
+    TextView tvNama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-
-
         getSupportActionBar().setTitle("Layout B");
+
+        tvNama = findViewById(R.id.tv_nama);
+
+        Intent terima = getIntent();
+        String ynama = terima.getStringExtra("inputNama");
+        tvNama.setText(ynama);
 
     }
 }
